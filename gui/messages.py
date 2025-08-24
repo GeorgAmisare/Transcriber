6e93389d7f1cedfa85dcecff32f263ba821fd83a
Egor <orlegand@gmail.com>
2025-08-24 07:44:27 +0700

    Update messages.py

--- gui/messages.py
+++ gui/messages.py
@@ -7,5 +7,5 @@ logger = logging.getLogger(__name__)
 
 READY_MESSAGE = "Перетащите в окно запись встречи для обработки"
 PROCESSING_MESSAGE = "Идёт обработка..."
-DONE_MESSAGE = "Обработка завершена, вы можете загрузить новую запись. Результат: "
+DONE_MESSAGE = "Обработка завершена"
 ERROR_MESSAGE = "Произошла ошибка"
